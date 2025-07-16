cdfb802699ff14e99c2f5fa9f5b9b1821c3b192c
Sai Kumar Nerella <nerellasaiyadav.1998@gmail.com>
2025-07-16 23:10:54 +0530

    code

--- s3/download_from_s3.py
+++ s3/download_from_s3.py
@@ -5,6 +5,7 @@ bucket_name = 'devops-demo-bucket-12345'
 key_name = 'uploads/region.py'
 download_path = './region.py'
 
+
 try:
     s3.download_file(bucket_name, key_name, download_path)
     print(f"Downloaded to {download_path}")
